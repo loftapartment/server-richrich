070e755d73fef29acd85bc08fbd542d488326184
Tina.Liang <teana0953@gmail.com>
2020-10-27 01:02:33 +0800

    Update user model

--- src/components/user/user.ts
+++ src/components/user/user.ts
@@ -1,3 +1,45 @@
 export interface IUser {
-    
+    /**
+     * email
+     * @description use email as account
+     */
+    email: string;
+
+    /**
+     *
+     */
+    password: string;
+
+    /**
+     * googleAuth
+     * @description google oAuth
+     */
+    googleAuth: boolean;
+
+    /**
+     *
+     */
+    name: string;
+
+    /**
+     *
+     */
+    role: string;
+
+    /**
+     * groupIds
+     * @class Group
+     */
+    groupIds: string[];
+
+    /**
+     * friendIds
+     * @class User
+     */
+    friendIds: string[];
+
+    /**
+     * image src
+     */
+    imageSrc: string;
 }
