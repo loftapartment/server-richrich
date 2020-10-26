551ea42b1d871f74d35be16934cb3cb160a6b9ce
Tina.Liang <teana0953@gmail.com>
2020-10-27 01:02:14 +0800

    Update server.ts, add connect db

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,24 @@
 import { app } from './app';
+import { DbService } from './services';
+import { Utility } from './helpers';
 
 let port: number = parseInt(process.env.PORT) || 3000;
 
-app.listen(port, () => {
-    console.log();
-    console.log(`App running on port \u001b[33m${port}\u001b[0m...`);
-    console.log();
-});
+(async () => {
+    try {
+        await DbService.connect();
+
+        console.log();
+        console.log(`connected to MongoDB \u001b[34m${DbService.baseUrl}\u001b[0m`);
+
+        app.listen(port, () => {
+            console.log();
+            console.log(`app running on port \u001b[33m${port}\u001b[0m...`);
+            console.log();
+        });
+    } catch (error) {
+        console.error(error);
+
+        Utility.killServer();
+    }
+})();
