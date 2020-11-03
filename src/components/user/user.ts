306a5ddf10e79a946a576dd72316a948e0cae508
Tina.Liang <teana0953@gmail.com>
2020-11-04 01:43:17 +0800

    Update user model

--- src/components/user/user.ts
+++ src/components/user/user.ts
@@ -1,45 +1,119 @@
-export interface IUser {
+import { IBase } from '../base-model';
+
+import { getModelForClass, ModelOptions, mongoose, prop } from '@typegoose/typegoose';
+import { IUser } from '.';
+
+export enum ERole {
+    Admin = 1,
+    ProUser,
+    User,
+}
+
+@ModelOptions({ schemaOptions: { timestamps: true, collection: 'User' } })
+export class UserClass {
     /**
      * email
+     * @unique
      * @description use email as account
      */
+    @prop()
     email: string;
 
     /**
      *
      */
+    @prop()
     password: string;
 
     /**
      * googleAuth
      * @description google oAuth
      */
-    googleAuth: boolean;
+    @prop({ default: false })
+    googleAuth?: boolean;
 
     /**
      *
      */
+    @prop()
     name: string;
 
     /**
      *
      */
-    role: string;
+    @prop({ default: ERole.User })
+    role?: ERole;
 
     /**
      * groupIds
      * @class Group
      */
-    groupIds: string[];
+    @prop()
+    groupIds?: string[];
 
     /**
      * friendIds
      * @class User
      */
-    friendIds: string[];
+    @prop()
+    friendIds?: string[];
 
     /**
      * image src
      */
+    @prop()
+    imageSrc?: string;
+
+    /**
+     *
+     */
+    @prop()
+    friendYouOweIds?: string[];
+
+    /**
+     *
+     */
+    @prop()
+    friendWhoOweIds?: string[];
+
+    /**
+     *
+     */
+    @prop()
+    groupYouOweIds?: string[];
+
+    /**
+     *
+     */
+    @prop()
+    groupWhoOweIds?: string[];
+
+    /**
+     * 
+     */
+    public static async validate(): Promise<void> {
+        try {
+
+        } catch (error) {
+            throw error;
+        }
+    }
+}
+
+export const User = getModelForClass(UserClass);
+
+//#region request
+
+//#endregion
+
+//#region response
+export interface IUserR {
+    id: string;
+    email: UserClass['email'];
+    name: UserClass['name'];
+    role: string;
+    groups: IBase.IResponse.IObject[];
+    friends: IBase.IResponse.IObject[];
     imageSrc: string;
 }
+//#endregion
