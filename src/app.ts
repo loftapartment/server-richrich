99afcd6ba7a9f859c6bc971062d0aa4e81d8e08a
Tina.Liang <teana0953@gmail.com>
2020-12-02 21:29:39 +0800

    Add cors

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,13 @@
 import bodyParser from 'body-parser';
 import Express from 'express';
 import { UserComponent } from './components';
+import Cors from 'cors';
 
 export const app = Express();
 
 app.disable('x-powered-by');
+
+app.use(Cors());
 
 app.use(Express.static(`${__dirname}/public`));
 
