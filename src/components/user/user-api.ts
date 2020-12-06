8e04d1fdc7fa33387807668ce38944e0b63dea10
Tina.Liang <teana0953@gmail.com>
2020-12-07 00:09:48 +0800

    Update user-api sign-up google

--- src/components/user/user-api.ts
+++ src/components/user/user-api.ts
@@ -1,8 +1,11 @@
 import express, { Request, Response } from 'express';
 import { IModel } from './model';
 import { UserComponent } from '../index';
-import { Utility } from '../../helpers';
+import { Utility, GoogleAuthHelper } from '../../helpers';
 import { Middleware } from '../../middlewares';
+import { UserController } from './user-controller';
+import { MongoData } from '../base-model/_index';
+import { IBase } from '../base-model';
 
 const _API_BASE = process.env.API_BASE;
 
@@ -36,16 +39,69 @@ UserApi
             let _input: IModel.IRequest.IUserC = res['input'];
 
             try {
+                /// google auth
                 if ('googleIdToken' in _input) {
-                    console.log(_input.googleIdToken);
+                    try {
+                        await GoogleAuthHelper.verify(_input.googleIdToken);
 
+                        // check whether already sign up
+                        let user: IModel.User = new IModel.User();
+                        await user.queryByField({
+                            equals: {
+                                email: _input.email
+                            }
+                        });
+
+                        if (!user.id) {
+                            /// create
+                            let data: IBase.MongoData<IModel.IUser> = {
+                                name: _input.name,
+                                role: IModel.ERole.User,
+                                email: _input.email,
+                                password: undefined,
+                                googleAuth: true
+                            };
+
+                            if ('gender' in _input) {
+                                data.gender = IModel.EGender[_input.gender];
+                            }
+
+                            if ('role' in _input) {
+                                data.role = IModel.ERole[_input.role];
+                            }
+
+                            if ('groupIds' in _input) {
+
+                            }
+
+                            if ('friendIds' in _input) {
+
+                            }
+
+                            if ('imageBase64' in user) {
+
+                            }
+
+                            user.data = data;
+
+                            await user.save();
+                        } else {
+                            /// update
+
+                        }
+
+                    } catch (error) {
+                        throw new Error(`google: ${error}`);
+                    }
                 } else {
+                    /// general
+
 
                 }
 
                 res.send(new Date());
             } catch (error) {
-                res.end(error instanceof Error ? error.message : error);
+                res.status(400).end(error instanceof Error ? error.message : error);
             }
         });
 
