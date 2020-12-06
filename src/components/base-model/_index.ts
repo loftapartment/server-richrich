6b755fdad09456ceb51489107b0e6247b4bdf8d7
Tina.Liang <teana0953@gmail.com>
2020-12-07 00:05:31 +0800

    Update base-model class save, query

--- src/components/base-model/_index.ts
+++ src/components/base-model/_index.ts
@@ -1,4 +1,5 @@
-import { ObjectId } from 'mongodb';
+import { Collection, ObjectId, OptionalId } from 'mongodb';
+import { DbService } from '../../../src/services';
 
 export type MongoData<T> = {
     _id?: ObjectId;
@@ -12,28 +13,51 @@ export interface IGetOptions<T> {
 }
 
 export class BaseCollection<T> {
-    private _id: string = undefined;
-    public get(): string {
+    protected _id: string = undefined;
+    public get id(): string {
         return this._id;
     }
 
-    private _data: T = undefined;
-    public get data(): T {
+    protected _collectionName: string = undefined;
+    public get collectionName(): string {
+        return this._collectionName;
+    }
+
+    private _data: MongoData<T> = undefined;
+    public get data(): MongoData<T> {
         return this._data;
     }
-    public set data(value: T) {
+    public set data(value: MongoData<T>) {
         this._data = value;
     }
 
-    constructor(data: T) {
-        this._data = data;
+    constructor()
+    constructor(id: string)
+    constructor(id?: string) {
+        this._id = id;
     }
 
     /**
      * save to db
      */
-    public save() {
+    public async save(): Promise<void> {
+        try {
+            // create
+            if (!this._id) {
+                this._data._created_at = new Date();
 
+                let result = await this.getCollection<T>(this.collectionName).insertOne(this._data as any);
+
+                this._id = result.insertedId.toHexString();
+
+            } else {
+                // update
+
+            }
+
+        } catch (error) {
+            throw error;
+        }
     }
 
     /**
@@ -43,6 +67,51 @@ export class BaseCollection<T> {
         if (!this._id) {
             throw new Error('_id can not empty');
         }
+    }
+
+    /**
+     * 
+     * @param data 
+     */
+    public async queryByField(options: IGetOptions<T>): Promise<void> {
+        try {
+            let query: any = {};
+            if (options.equals) {
+                Object.keys(options.equals).forEach((key) => {
+                    query[`${key}`] = {
+                        $eq: options.equals[key]
+                    }
+                })
+            }
+
+            if (options.notEquals) {
+                Object.keys(options.equals).forEach((key) => {
+                    query[`${key}`] = {
+                        $ne: options.notEquals[key]
+                    }
+                })
+            }
+
+            let result: MongoData<T> = await this.getCollection<T>(this.collectionName).findOne(query);
+
+            if (!!result) {
+                this.data = result;
+
+                this._id = result._id.toHexString();
+            }
+
+        } catch (error) {
+            throw error;
+        }
+    }
+
+    /**
+     * 
+     * @param name 
+     */
+    private getCollection<T>(name: string): Collection<MongoData<T>> {
+        let db = DbService.db;
+        return db.collection(name);
     }
 }
 
