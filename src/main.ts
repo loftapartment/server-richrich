80a50bc12e19a106e3fb20816d7ab29d5b81bf83
Tina.Liang <teana0953@gmail.com>
2020-12-23 00:18:26 +0800

    Create default user

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,7 @@ import './app';
 import { app } from './app';
 import { DbService } from './services';
 import { Utility } from './helpers';
+import { UserComponent } from './components';
 
 let port: number = parseInt(process.env.PORT) || 3000;
 
@@ -13,6 +14,23 @@ let port: number = parseInt(process.env.PORT) || 3000;
 
         console.log();
         console.log(`connected to MongoDB \u001b[34m${DbService.baseUrl}\u001b[0m`);
+
+        // create default
+        let user = new UserComponent.IModel.User();
+        await user.queryByField({
+            equals: {
+                name: 'Admin'
+            }
+        });
+        if (!user.id) {
+            await UserComponent.UserController.signUp({
+                name: 'Admin',
+                email: 'admin@richrich.com',
+                password: process.env.ADMIN_DEFAULT_PW
+            }, UserComponent.IModel.ERole.Admin);
+
+            console.log('create default user');
+        }
 
         app.listen(port, () => {
             console.log();
